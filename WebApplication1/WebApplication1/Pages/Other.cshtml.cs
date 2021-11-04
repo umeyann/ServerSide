658f5bf5e611fbac4e47b7e5bc501b04ce5999bc
umeyann <umemura.sigoto@gmail.com>
2021-11-04 16:37:28 +0900

    重大度レベル コード 説明 プロジェクト ファイル 行 抑制状態 エラー CS0246 型または名前空間の名前 'Platform' が見つかりませんでした (using ディレクティブまたはアセンブリ参照が指定されていることを確認してください) WebApplication1 D:\5_School\4 years\2_[吉川 翔太]Sever Side\Project\WebApplication1\WebApplication1\Pages\Other.cshtml 39 アクティブ エラー CS0103 現在のコンテキストに 'Gender' という名前は存在しません WebApplication1 D:\5_School\4 years\2_[吉川 翔太]Sever Side\Project\WebApplication1\WebApplication1\Pages\Other.cshtml 19 アクティブ エラー CS0103 現在のコンテキストに 'Gender' という名前は存在しません WebApplication1 D:\5_School\4 years\2_[吉川 翔太]Sever Side\Project\WebApplication1\WebApplication1\Pages\Other.cshtml 25 アクティブ エラー CS0246 型または名前空間の名前 'Platform' が見つかりませんでした (using ディレクティブまたはアセンブリ参照が指定されていることを確認してください) WebApplication1 D:\5_School\4 years\2_[吉川 翔太]Sever Side\Project\WebApplication1\WebApplication1\Pages\Other.cshtml 32 アクティブ

--- WebApplication1/WebApplication1/Pages/Other.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Other.cshtml.cs
@@ -7,16 +7,39 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SampleRazorApp.Pages
 {
+    public enum Gender
+    {
+        male,
+        female
+    }
+    public enum Platform
+    {
+        Windows,
+        macOS,
+        Linux,
+        ChromeOS,
+        Android,
+        iOS
+    }
     public class OtherModel : PageModel
     {
         public string Message { get; set; }
-        public void OnPost()
-        {
-            Message = "You typed:" + Request.Form["msg"];
-        }
+        public bool Check { get; set; }
+        public Gender Gender { get; set; }
+        public Platform pc { get; set; }
+        public Platform[] pc2 { get; set; }
         public void OnGet()
         {
-
+            Message = "check & selecte it!";
+        }
+        public void OnPost(bool check, string gender, Platform pc, Platform[] pc2)
+        {
+            Message = "Result:" + check + "," + gender + "," + pc + ",";
+            foreach (Platform pf in pc2)
+            {
+                Message += pf + ",";
+            }
+            Message += "(" + pc2.Length + ")";
         }
     }
 }
